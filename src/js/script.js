b8258881719ee29a38b360b09c279d65aeb42a4e
Ivan <imarcbenki@gmail.com>
2022-07-22 14:40:11 +0300

    problem with carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 992,
                 settings: {
                     dots: true,
