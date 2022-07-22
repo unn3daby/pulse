4e198fb0eee4d98a91e10e7db510e0c82c7a15a2
Ivan <imarcbenki@gmail.com>
2022-07-22 15:59:17 +0300

    fixed

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [{
+        responsive: [
+            {
                 breakpoint: 992,
                 settings: {
                     dots: true,
